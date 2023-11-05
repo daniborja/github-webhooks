304dfdf0d88ebc5f37cf47ecd950ec2382ed1682
Adrian <alextravc.342@gmail.com>
2023-11-05 14:20:10 -0500

    [ADD]: GitHubStarPayload & GitHubIssuePayload

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
-const nameUser = 'Alex';
+import express from 'express';
 
-console.log(`Hi ${nameUser}`);
+import { envs } from './config';
+import { GitHubController } from './presentation/github/github.controller';
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+
+  app.use(express.json());
+
+
+  const controller = new GitHubController();
+
+  app.post('/api/github', controller.webhookHandler);
+
+  app.listen(envs.PORT, () => {
+    console.log(`Server is running on port ${envs.PORT}`);
+  });
+}
